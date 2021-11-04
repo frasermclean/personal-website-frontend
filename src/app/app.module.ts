1b8771726eb68183df8f229aa57ba03cc03653ea
Fraser McLean <dev@frasermclean.com>
2021-11-04 13:34:44 +1100

    Redesigned welcome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+// angular modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// application modules
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
@@ -13,8 +17,9 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
